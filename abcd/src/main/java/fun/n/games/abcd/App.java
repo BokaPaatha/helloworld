625d29bc41c0e7b6798f2a6d1762cc06d1a2349e
thebrutepoint <thebrutepoint@gmail.com>
2015-11-14 23:53:29 +0530

    abcd

---
+++ abcd/src/main/java/fun/n/games/abcd/App.java
@@ -0,0 +1,13 @@
+package fun.n.games.abcd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
